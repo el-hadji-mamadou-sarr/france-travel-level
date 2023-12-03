32994bd07cb025ec8ab35042c34ab333148b54c3
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2023-12-03 20:42:34 +0100

    fix deployement

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import { openDB } from './services/indexDB'
 import { onBeforeMount } from 'vue'
 onBeforeMount(() => {
